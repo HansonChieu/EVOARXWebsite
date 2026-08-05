f31662f0e208f44a0c67d591fc8d43bcb0cc196b
Hanson <hansonchieu@hotmail.com>
2026-08-05 19:05:33 -0400

    Add routes for all pages

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import PrivacyPolicy from './pages/PrivacyPolicy.tsx'
+import TermsOfService from './pages/TermsOfService.tsx'
+import Contact from './pages/Contact.tsx'
+import AboutUs from './pages/AboutUs.tsx'
+import Store from './pages/Store.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
